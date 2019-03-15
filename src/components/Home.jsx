8d68c2faeacf679495eb1a9a0a97038bf6df4fc3
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2019-03-15 12:04:09 -0700

    Updates and restarting progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Container, Button } from 'reactstrap';
 import './Home.css';
+//import {ipcRenderer} from 'electron';
+//const { ipcRenderer } = window.require('electron');
+
+const electron = window.require('electron');
+//const fs = electron.remote.require('fs');
+const ipcRenderer  = electron.ipcRenderer;
+
 
 export default class Home extends Component {
+  
+
+  componentDidMount() {
+    loadPage();
+
+    function loadPage(){
+      ipcRenderer.send('employee-get', 'ping')
+    }
+    ipcRenderer.on('employee-reply', (event, arg) => {
+      document.getElementById('dbtest').innerHTML = arg
+      console.log(arg)
+    })
+  }
+
   render() {
     return (
       <Container>
@@ -22,6 +43,11 @@ export default class Home extends Component {
 
             <h4>Notes</h4>
             <p>using the <code>&lt;link&gt;</code> tags is like using the <code>&lt;a&gt;</code> tag in react-router-dom</p>
+
+            <h3 id="dbtest"> </h3>
+
+
+            
             <Link to="/">
               <Button color="primary"> Go to next page </Button>
             </Link>
