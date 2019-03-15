08327391e3218caa86bd0746758c0d5efe52b92f
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2019-03-15 12:24:33 -0700

    Updated to-do

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Container, Button } from 'reactstrap';
 import './Home.css';
-//import {ipcRenderer} from 'electron';
-//const { ipcRenderer } = window.require('electron');
 
 const electron = window.require('electron');
 //const fs = electron.remote.require('fs');
@@ -33,18 +31,27 @@ export default class Home extends Component {
 
             <h3>To do list: </h3>
             <p>
-            1. 
+              1. Create pages and navigation <br/>
+              2. Create deployable react build using <code>npm run build</code> and electron-builder<br/>
+              3. Create db structure<br/>
+              4. Implement functionality on data entry and retrieval<br/>
+              5. Implemet search<br/>
+              6. Implement queries page<br/>
             </p>
 
             <h3>Optional: </h3>
             <p>
-            1. 
+            1. Make pretty graphics
             </p>
 
             <h4>Notes</h4>
             <p>using the <code>&lt;link&gt;</code> tags is like using the <code>&lt;a&gt;</code> tag in react-router-dom</p>
 
-            <h3 id="dbtest"> </h3>
+            <hr/>
+
+
+            <h4>Sample data retrieval:</h4>
+            <p id="dbtest"> </p>
 
 
             
